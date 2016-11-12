59135d44a821b82274255ce55d1f08a01ea51416
Louis Buckalew <louisbuckalew@Louis-MacBook-Pro.local>
2016-11-11 21:20:11 -0600

    added error checking handling for shell commands

--- blulang.go
+++ blulang.go
@@ -1,107 +1,107 @@
 package blulang
 
-import "os/exec"
-import "io"
-import "io/ioutil"
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os/exec"
+)
 
-type Adapter struct {
-	Name string
-	Powered, Discoverable, Pairable, Discovering bool
-	btshell btShell
-}
-
-type Device struct {
-	Name string
-}
-
-
-
-
-
-
-
-type btShell struct {
-	stdin io.Writer
+type bluetoothctl struct {
+	cmd    *exec.Cmd
+	stdin  io.Writer
 	stdout io.Reader
 }
 
-func (b *btShell) start() error {
+func (b *bluetoothctl) start() error {
+	var err error
 
-	btCtl := exec.Command("bluetoothctl")
+	b.cmd = exec.Command("bluetoothctl")
 
-	b.stdin, _ = btCtl.StdinPipe()
-	b.stdout, _ = btCtl.StdoutPipe()
+	b.stdin, err = b.cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
 
-	btCtl.Start()
-	return nil
+	b.stdout, err = b.cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	return b.cmd.Start()
 }
 
-func (b *btShell) write(btCmd string) error {
+func (b *bluetoothctl) write(btCmd string) error {
+
 	b.stdin.Write([]byte(btCmd))
+
 	return nil
 }
 
-func (b *btShell) read(btMsg string) (string, error) {
+func (b *bluetoothctl) read(btMsg string) (string, error) {
+
 	buff, error := ioutil.ReadAll(b.stdout)
+
 	return string(buff), error
 }
 
-func (b *btShell) stop() error {
+func (b *bluetoothctl) stop() error {
+
+	b.cmd.Wait()
+
 	b.write("exit")
+
 	return nil
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+type Adapter struct {
+	Name                                         string
+	Powered, Discoverable, Pairable, Discovering bool
+	shell                                        bluetoothctl
+}
 
 /* Attempts to connect to any known hosts
-*/
+ */
 func (a Adapter) ConnectKnown() error {
 
 	// av_devs = adapter.startScan()
 	// bd_devs = adapter.getBondedDevices()
 	// if av_dev in bd_devs
-		// connect
+	// connect
 	// error
 	return nil
 }
 
 /* Makes controller invisible and unpairable
-*/
+ */
 func (a Adapter) Lock() error {
+
 	return nil
 }
 
 /* Allows for device to make pair request and connect
-*/
+ */
 func (a Adapter) FindDevice() error {
+
 	return nil
 }
 
 func (a *Adapter) Testmsg() {
-	blob, error := ioutil.ReadAll(a.btshell.stdout)
+
+	blob, error := ioutil.ReadAll(a.shell.stdout)
+
 	fmt.Println("t")
 	fmt.Println(string(blob))
 	fmt.Println(error)
 }
 
 func (a *Adapter) GetAdapter() (Adapter, error) {
+
 	// check for btshell
-	a.btshell.start()
+	a.shell.start()
+
 	// enable
+
 	// get attributes from shell
 	a.Name = "Test name"
 	a.Powered = false
@@ -113,11 +113,16 @@ func (a *Adapter) GetAdapter() (Adapter, error) {
 }
 
 func (a *Adapter) enable() error {
+
 	return nil
 }
 
 func (a *Adapter) disable() error {
+
 	return nil
 }
 
-
+type Device struct {
+	Name    string
+	Address string
+}
